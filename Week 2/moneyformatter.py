03a31065f1660845cacf12d62a05f39333d2e7ff
Marcus Persson <marcusper2003@gmail.com>
2023-09-06 16:54:12 +0200

    Elaborate on moneyformatter.py

--- Week 2/moneyformatter.py
+++ Week 2/moneyformatter.py
@@ -9,6 +9,9 @@ Description:
 print("Please enter a number. Preferably a float!")
 money_value = float(input("Your number: "))
 euro = int(money_value)
+# For the cent, I subtracted money_value with the new int euro, leaving only the decimal.
+# This is then multiplied by 100 to create a whole number (assuming we aren't dealing with thousandths and beyond).
+# It is then rounded and converted to an integer. I had to round first due to the behaviour the int function does.
 cent = int(round((money_value - euro) * 100))
 
 print(euro, "euro", cent)
