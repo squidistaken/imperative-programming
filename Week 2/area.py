dfc16bae8fce086e9c424fbc8235c60962eaba53
Marcus Persson <marcusper2003@gmail.com>
2023-09-06 23:12:32 +0200

    area.py Prep

---
+++ Week 2/area.py
@@ -0,0 +1,15 @@
+'''
+File: area.py
+Name: Marcus Persson (m.h.o.persson@student.rug.nl)
+
+Description:
+    This program
+'''
+
+def get_area_of_triangle(base, height):
+    area = (base * height) / 2
+    return area
+
+def get_area_of_rectangle(base, height):
+    area = base * height
+    return area
