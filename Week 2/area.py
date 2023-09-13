3f8fa6395f4d577a265426ef7f3ef7564df2c627
Marcus Persson <marcusper2003@gmail.com>
2023-09-13 17:45:25 +0200

    Week 2

--- Week 2/area.py
+++ Week 2/area.py
@@ -3,13 +3,29 @@ File: area.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    This program
+    This program calculates the area of a triangle or rectangle, depending on what is specified.
 '''
 
-def get_area_of_triangle(base, height):
-    area = (base * height) / 2
+
+def get_area_of_triangle(b, h):
+    area = (b * h) / 2
     return area
 
-def get_area_of_rectangle(base, height):
-    area = base * height
+
+def get_area_of_rectangle(b, h):
+    area = b * h
     return area
+
+'''
+shape_input = str(input())
+base = float(input())
+height = float(input())
+
+match shape_input:
+    case "triangle":
+        print(get_area_of_triangle(base, height))
+    case "rectangle":
+        print(get_area_of_rectangle(base, height))
+    case other:
+        print("Shape not found.")
+'''
