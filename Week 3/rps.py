eb567c84d61dec2ce6032ea5c50c6b5163360988
Marcus Persson <marcusper2003@gmail.com>
2023-09-19 11:49:40 +0200

    rps.py: First implementation

---
+++ Week 3/rps.py
@@ -0,0 +1,26 @@
+'''
+File: area.py
+Name: Marcus Persson (m.h.o.persson@student.rug.nl)
+
+Description:
+    This program emulates rock, paper, scissors.
+'''
+
+option_1 = str(input())
+option_2 = str(input())
+
+match option_1, option_2:
+    case "rock", "scissors":
+        print("Player one wins.")
+    case "rock", "paper":
+        print("Player two wins.")
+    case "paper", "scissors":
+        print("Player two wins.")
+    case "paper", "rock":
+        print("Player one wins.")
+    case "scissors", "rock":
+        print("Player two wins.")
+    case "scissors", "paper":
+        print("Player one wins.")
+    case default:
+        print("It is a draw.")
