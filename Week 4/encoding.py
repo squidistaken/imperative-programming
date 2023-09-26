0f51240ccb1f407d9cc3ac56ad757075ce083f4c
Marcus Persson <marcusper2003@gmail.com>
2023-09-26 12:38:00 +0200

    encoding.py: setup

--- Week 4/encoding.py
+++ Week 4/encoding.py
@@ -3,5 +3,28 @@ File: name.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    Description
+    This program is a simple form of encoding exercise.
 '''
+
+input = list(input())
+
+'''
+Rules:
+- Uppercase characters stay uppercase characters, and lowercase characters stay lowercase characters.
+- Non-letter characters should not be encoded.
+'''
+
+'''
+ex. a = b (+1)
+
+b = d (+2)
+'''
+
+# TODO: THE ENCODING
+
+# while input_length < len(input):
+    # input[input_length]
+
+output = ''.join(input)
+
+print(output)
