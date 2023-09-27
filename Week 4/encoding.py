7a5323dca9decc09ac734ebe669ea2672f62261a
Marcus Persson <marcusper2003@gmail.com>
2023-09-28 00:59:21 +0200

    update: tweaking

--- Week 4/encoding.py
+++ Week 4/encoding.py
@@ -1,24 +1,62 @@
-'''
+"""
 File: name.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    This program is a simple form of encoding exercise.
-'''
+    This program is a simple form of an encoding exercise.
+"""
 
-lst_input = list(input())
-
-'''
+"""
 Rules:
 - Uppercase characters stay uppercase characters, and lowercase characters stay lowercase characters.
 - Non-letter characters should not be encoded.
-'''
+"""
+
+#
+lst_input = list(input())
+lst_output = []
 
 index = 0
 
+# 33 = !
+# 46 = .
+# 63 = ?
+# 32 =  (the space)
+
+# 65-90 = A-Z
+# 97-122 = a-z
 while index < len(lst_input):
-    # do stuff
+    lst_index_unicode = ord(lst_input[index])
+    # if char_unicode == range(65, 90) or range(97, 122) or 36 or 46 or 63:
+    print(lst_index_unicode)
+    if 65 > lst_index_unicode < 97:
+        print("Invalid; no non-letter characters should be encoded.")
+        break
+    else:
+        lst_index_unicode += index + 1
 
-lst_output = ''.join(lst_input)
+        # TODO: figure out the rest :(
 
-print(lst_output)
+        '''
+        if lst_index_unicode > 122:
+            lst_index_unicode -= 26
+            lst_index_unicode += index + 1
+        else:
+            lst_index_unicode += index + 1
+        '''
+    index += 1
+
+    lst_output.append(chr(lst_index_unicode))
+
+print(''.join(lst_output))
+
+'''
+Input: abcdefghijklmnopqrstuvwxyz
+Output: bdfhjlnprtvxzbdfhjlnprtvxz
+
+Input: Hello World!
+Output: Igopt Dwavo!
+
+Input: What's in a name? That which we call a rose...
+Output: Xjdx'y qw l aobu? Mbvp ugidj aj jiuv m fdiv...
+'''
--- Week 4/primes.py
+++ Week 4/primes.py
@@ -1,15 +1,38 @@
-'''
+"""
 File: name.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
     Description
-'''
+"""
 
 x = int(input())
 flag = False
 
+
 if x < 2:
     print("Invalid response; x must be greater than 2")
 else:
-    # do stuff
+    x -= 1
+    while not flag:
+        print(x)
+        # TODO: Tweak the conditional!
+        if (x % 2 == 0) or (x % 3 == 0) or (x % 5 == 0) or (x % 31 == 0) or (x % 41 == 0) or (x % 109 == 0) or (x % 11 == 0) or (x % 53 == 0):
+            x -= 1
+        else:
+            print(x)
+            flag = True
+            break
+
+
+"""
+Input:  1234
+Output: 1231
+
+Input:  11
+Output: 7
+
+- todo
+Input:  123456789
+Output: 123456761
+"""
