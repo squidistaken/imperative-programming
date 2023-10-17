1add8ed84ed5d94b18494369b0a34003c820950e
Marcus Persson <marcusper2003@gmail.com>
2023-10-17 10:44:19 +0200

    Setup for week 7

---
+++ Week 7/mazerunner.py
@@ -0,0 +1,7 @@
+"""
+File: mazerunner.py
+Name: Marcus Persson (m.h.o.persson@student.rug.nl)
+
+Description:
+    Description
+"""
---
+++ Week 7/missingno2.py
@@ -0,0 +1,12 @@
+"""
+File: missingno2.py
+Name: Marcus Persson (m.h.o.persson@student.rug.nl)
+
+Description:
+    Description
+"""
+
+# The first input contains an unknown number of integers.
+lst = [int(_) for _ in input().split(" ")]
+# The second input is the maximum number k of values you are allowed to remove from this sequence.
+k = int(input())
