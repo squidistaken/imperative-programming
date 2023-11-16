26aaaa5e789b058578cf0cf8b24fdb8c905aa206
Marcus Persson <marcusper2003@gmail.com>
2023-11-16 17:39:17 +0100

    Planning

--- Block 2/Week 1/numbers.py
+++ Block 2/Week 1/numbers.py
@@ -5,3 +5,24 @@ Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 Description:
     Description
 """
+
+"""
+Outline:
+- 
+"""
+
+
+# TODO: Find largest possible arithmetic series
+def add_nums(start: int, limit: int, step: int) -> int:
+    if start >= limit:
+        return start
+    start += step
+    return add_nums(start, limit, step)
+
+
+# Input for this exercise is a line with three numbers n, m, and k.
+n, m, k = map(int, input().split())
+
+# Output is the sum of the numbers from n until m (both inclusive) in steps of size k.
+total = add_nums(n, m, k)
+print(total)
--- Block 2/Week 1/suitcase.py
+++ Block 2/Week 1/suitcase.py
@@ -5,3 +5,17 @@ Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 Description:
     Description
 """
+
+
+n, v = map(int, input().split())
+
+while True:
+    if input() == "":
+        break
+    item, item_size, item_satisfaction = map(int, input().split())
+    # TODO: Build dictionary here
+
+
+def find_optimal_satisfaction() -> int:
+    # TODO: Find optimal satisfaction
+    pass
