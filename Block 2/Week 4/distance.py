0c74bdc9b41935ea035acf83feda9bd7378167a7
Marcus Persson <marcusper2003@gmail.com>
2023-12-08 16:02:40 +0100

    other approaches

--- Block 2/Week 4/distance.py
+++ Block 2/Week 4/distance.py
@@ -1,9 +1,9 @@
 """
-File: distance.py
+File: distance_old_v1.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    This returns the the number of unique pairs (i, j) in a sequence that are exactly k apart.
+    This returns the number of unique pairs (i, j) in a sequence that are exactly k apart.
 """
 
 # First input is the number k
@@ -11,37 +11,6 @@ k = int(input())
 # Second input is a sequence of integer numbers
 integers = [int(_) for _ in input().split(" ")]
 integers = list(dict.fromkeys(integers))
-
-MAX_NUMBER = 1000000000
-RADIX = 10
-
-
-def counting_sort(sequence: list[int], factor: int = 1) -> None:
-    output = [0] * len(sequence)
-    tally = [0] * RADIX
-    for value in sequence:
-        tally[(value // factor) % RADIX] += 1
-    for i in range(1, RADIX):
-        tally[i] += tally[i - 1]
-    position = len(sequence) - 1
-    while position >= 0:
-        tally_position = (sequence[position] // factor) % RADIX
-        tally[tally_position] -= 1
-        output[tally[tally_position]] = sequence[position]
-        position -= 1
-    for position in range(len(sequence)):
-        sequence[position] = output[position]
-
-
-def radix_sort(sequence: list[int]) -> None:
-    factor = 1
-    while factor < MAX_NUMBER:
-        counting_sort(sequence, factor)
-        factor *= RADIX
-
-
-radix_sort(integers)
-
 
 count = 0
 for i in range(len(integers)):
---
+++ Block 2/Week 4/forces_old_v2.py
@@ -0,0 +1,45 @@
+"""
+File: forces_old_v2.py
+Name: Marcus Persson (m.h.o.persson@student.rug.nl)
+
+Description:
+    This program merges an unknown number of sorted sequences together, keeping them sorted.
+"""
+sequences = []
+
+# Input is an unknown number of sorted sequences of unknown length.
+# Each input sequence is on its own line.
+# Input ends with an empty line.
+while True:
+    integers = input()
+
+    if integers == "":
+        break
+
+    integers = [int(_) for _ in integers.split(" ")]
+    sequences.append(integers)
+
+
+def get_position(sequence: list[int], n: int) -> int:
+    pos = 0
+
+    for i in range(len(sequence)):
+        if n > sequence[i]:
+            pos += 1
+
+    return pos
+
+
+def merge_sorted(sequence: list[list[int]]) -> list[int]:
+    new_sequence = []
+
+    for i in range(len(sequence)):
+        for j in range(len(sequence[i])):
+            new_sequence.insert(get_position(new_sequence, sequence[i][j]), sequence[i][j])
+
+    return new_sequence
+
+
+final_sequence = merge_sorted(sequences)
+
+print(*final_sequence)
