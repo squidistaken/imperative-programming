ec7cc57dc709dd251d441ed81a23b013272b5745
Marcus Persson <marcusper2003@gmail.com>
2023-12-15 13:35:09 +0100

    hw done

--- Block 2/Week 5/disc.py
+++ Block 2/Week 5/disc.py
@@ -3,27 +3,26 @@ File: disc.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    Description
+    This program counts all possible grid points in a circle with r radius.
 """
 # Input is a single integer number r, which represents the radius of a disc with center (0, 0).
 r = int(input())
 
-# TODO: Optimise to O(nlog(n))?
-def count_grid_points(radius: int) -> int:
-    count = 0
-    x, y = radius, radius
-    # O(n)
-    while x >= 0:
-        if y == 0:
-            break
-        if x * x + y * y <= radius * radius:
-            count += 1
-        if x == 0:
-            x = radius
-            y -= 1
-        x -= 1
 
-    return 1 + (4 * count)
+def count_grid_points(radius: int) -> int:
+    axis_count = 1 + 4 * radius
+    count = 0
+    x = 1
+    y = radius
+
+    while x <= radius:
+        if (x * x + y * y) > radius * radius:
+            y -= 1
+        else:
+            count += y
+            x += 1
+
+    return (count * 4) + (1 + (4 * radius))
 
 
 # Output is the number of integer grid points that are contained within this disc (including the edge).
--- Block 2/Week 5/road.py
+++ Block 2/Week 5/road.py
@@ -3,7 +3,7 @@ File: road.py
 Name: Marcus Persson (m.h.o.persson@student.rug.nl)
 
 Description:
-    Description
+    This program finds the shortest distance between any pair of villages among N villages.
 """
 
 # The input is a line with the number of villages N
@@ -17,4 +17,27 @@ while n < N:
     grid.append([int(_) for _ in input().split(" ")])
     n += 1
 
+
+# Floyd-Warshall's Algorithm
+def find_shortest_distances(matrix: list[list[int]]) -> list[list[int]]:
+    new_grid = matrix
+    length = len(new_grid)
+
+    for k in range(length):
+        for i in range(length):
+            for j in range(length):
+                # Don't update state if i and j are the same.
+                if i == j:
+                    continue
+                # Zero = Impossible to reach
+                if ((new_grid[i][j] == 0 or new_grid[i][j] > new_grid[i][k] + new_grid[k][j])
+                        and new_grid[i][k] != 0 and new_grid[k][j] != 0):
+                    new_grid[i][j] = new_grid[i][k] + new_grid[k][j]
+                    new_grid[j][i] = new_grid[i][k] + new_grid[k][j]
+
+    return new_grid
+
+
 # Output is N lines that represent the shortest distance between each of the villages.
+for row in find_shortest_distances(grid):
+    print(*row)
